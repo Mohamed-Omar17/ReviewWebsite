473d2941b47ba56b041ce257c173fb9afad9376d
RayanK05 <rayan.khalil@ontariotechu.net>
2024-11-23 21:58:04 -0500

    Ayaan's pages and images for front and back

--- src/Javascript/scroll.js
+++ src/Javascript/scroll.js
@@ -1,11 +1,13 @@
-const backBtn = document.querySelector('.backBtn');
-const frontBtn = document.querySelector('.frontBtn');
-const carousel = document.querySelector('.carousel .card-list');
+document.querySelectorAll('.container').forEach((container) => {
+    const backBtn = container.querySelector('.backBtn');
+    const frontBtn = container.querySelector('.frontBtn');
+    const carousel = container.querySelector('.carousel .card-list');
 
-backBtn.addEventListener('click', () => {
-    carousel.scrollBy({ left: -200, behavior: 'smooth' });
-});
+    backBtn.addEventListener('click', () => {
+        carousel.scrollBy({ left: -200, behavior: 'smooth' });
+    });
 
-frontBtn.addEventListener('click', () => {
-    carousel.scrollBy({ left: 200, behavior: 'smooth' });
+    frontBtn.addEventListener('click', () => {
+        carousel.scrollBy({ left: 200, behavior: 'smooth' });
+    });
 });
