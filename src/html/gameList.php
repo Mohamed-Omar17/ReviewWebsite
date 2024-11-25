6b704ea3abedd09b9558b8b015d52ece1d1866ac
zachmatt02 <zachmatasic@gmail.com>
2024-11-24 21:26:50 -0500

    Added Game list style
    Change gameList.php to
    -Take image and surrounding block as a link instead of button
    -Display the Image of the game and the average score
    Changed the CSS to be inline with website

--- src/html/gameList.php
+++ src/html/gameList.php
@@ -3,9 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>XP Game Reviews</title>
-    <link rel="stylesheet" href="/css/header.css">
-    <link rel="icon" type="image/svg" href="../images/1.svg">
-    <link rel="stylesheet" href="/css/gameList.css">
+  <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/gameList.css">
     <link href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -30,6 +29,9 @@
                     <li class="link">
                         <a href="addReview.php">Add Review</a>
                     </li>
+                    <li class="link">
+                        <a href="">Sign In</a>
+                    </li>
                 </div>
             </ul>
             <button class="menu-button">
@@ -45,29 +47,55 @@
 require_once('config.inc.php');
 $mysqli = new mysqli(HOST, USER, PASSWORD, DB, PORT);
 
-$SQL="SELECT * FROM GameNames ORDER BY Game";
+$SQL = "SELECT * FROM GameNames ORDER BY Game";
 
 echo "<div id='pageBody'>";
 
-if ($mysqli -> connect_error) {
-    die("Connection failed");
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
 }
 
-if ($result = $mysqli -> query($SQL)) {
-    while ($obj=$result->fetch_object()) {
-        $gameName = $obj->Game;
+if ($result = $mysqli->query($SQL)) {
+    while ($obj = $result->fetch_object()) {
+        $gameName = htmlspecialchars($obj->Game);
+        $publisher = htmlspecialchars($obj->Publisher);
+        $imagePath = htmlspecialchars($obj->CoverImageData);
+
+        // Query for Average Score
+        $SQLAvg = "SELECT AVG(Score) AS AverageScore FROM Reviews WHERE Game = '$gameName'";
+        $resultAvg = $mysqli->query($SQLAvg);
+
+        $averageScore = 0; 
+        if ($resultAvg) {
+            $row = $resultAvg->fetch_assoc();
+            if (!is_null($row['AverageScore'])) {
+                $averageScore = round($row['AverageScore'], 2); 
+            }
+        } else {
+            echo "<p>Error calculating average score for $gameName</p>";
+        }
+
+
         echo "<div class='games'>";
-        echo "<p>Game: " . htmlspecialchars($obj->Game) . "</p><br>";
-        echo "</p>";
-        echo "<p>" . htmlspecialchars($obj->Publisher) . "</p><br>";
-        echo "<button><a href='game-info.php?game=" . $gameName . "'>Reviews</a></button>";
+        echo "<a href='game-info.php?game=" . urlencode($gameName) . "' style='text-decoration: none; color: white; display: block; height: 100%;'>";
+        echo "<p class = 'nameText'>Game: " . $gameName . "</p>";
+        echo "<img src='" . $imagePath . "' alt='" . $gameName . " Image' class='game-image'/>";
+        echo "<p>Publisher: " . $publisher . "</p>";
+        echo "<p class='average-score-box'>" . $averageScore . "</p>"; 
+        echo "</a>"; 
         echo "</div>";
     }
-    $result -> free_result();
+    $result->free_result();
 }
+
 echo "<div id='footer'>";
-echo "<footer><button type='button' class='btn btn-primary'><a href='addReview.php' class='reviewButton'>Add new Review</a></button></footer>";
+echo "<footer><a href='addReview.php'><p></p><p>Add Your Own Review</p></a></footer>";
 echo "</div>";
 echo "</div>";
+
 ?>
+
+
+
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
